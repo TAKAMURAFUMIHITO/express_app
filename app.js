4bc02397f741994ac6de23e57af56a006203f6c2
TAKAMURAFUMIHITO <108682238+TAKAMURAFUMIHITO@users.noreply.github.com>
2023-02-15 12:20:08 +0900

    レコードの作成, 更新

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
+const dbRouter = require('./routes/db');
 
 var app = express();
 const ses_opt = {
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
+app.use('/db', dbRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
